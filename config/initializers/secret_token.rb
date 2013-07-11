7b5c7e5d3fec959bd5df28caca4d9e769c3fc904
Wesley Carr <surfswell17@gmail.com>
2013-07-11 15:34:19 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InstaApp::Application.config.secret_token = '83bf4eb79a1fb2d38c93e0e3a88126dbf1f83013c9e99f1fb36ac543c23d74d1893e823c71506ba2387b04a3b63325e642bc89fc19a52bf5594fff55dbd5488d'
